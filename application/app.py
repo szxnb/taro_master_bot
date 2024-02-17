3db7e5302528a263404ca3300d95fe91cd7423a5
zayn <3500466989@qq.com>
2024-02-18 03:04:45 +0800

    通过flask将生成报告方法做成了API

---
+++ application/app.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from main import MainProcess
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.get('/make_report/<user_question>/<card1>/<card2>/<card3>/<user_email>')
+def make_report(user_question, card1, card2, card3, user_email):
+    print("收到参数:" + user_question + "/" + card1 + "/" + card2 + "/" + card3 + "/" + user_email)
+    question = user_question
+    card = f"{card1},{card2},{card3}"
+    email = user_email
+
+    main_process_instance = MainProcess()
+    main_process_instance.set_taro_user_question(question)
+    main_process_instance.set_taro_user_card(card)
+    main_process_instance.set_taro_user_email(email)
+    main_process_instance.start_process()
+    return f"<p>生成完成，报告已发送至{email}邮箱，请查收</p>"
+
+# /make_report/我什么时候能结婚/战车/力量/月亮/3500466989@qq.com
